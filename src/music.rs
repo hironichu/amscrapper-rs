7523e228d5049a37b1a791a4bf2ed70d1e185828
Your Name <you@example.com>
2025-03-18 00:23:40 +0100

    update

--- src/music.rs
+++ src/music.rs
@@ -18,27 +18,3 @@ pub struct AMusicState {
     pub playing: bool,
     pub live: bool,
 }
-impl AMusicState {}
-
-impl Drop for AMusicSongInfo {
-    fn drop(&mut self) {
-        self.song = String::new();
-        self.artist = String::new();
-        self.album = String::new();
-    }
-}
-
-impl Drop for AMusicTimeInfo {
-    fn drop(&mut self) {
-        self.duration = 0;
-        self.remaining_duration = 0;
-        self.current_time = 0;
-        self.total = 0;
-    }
-}
-impl Drop for AMusicState {
-    fn drop(&mut self) {
-        self.playing = false;
-        self.live = false;
-    }
-}
